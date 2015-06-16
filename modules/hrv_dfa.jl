02bc845f254507237d5e4f37adec0443dc6f7d4d
Michał <mchrz34@gmail.com>
2015-06-16 19:26:55 +0000

    hrv_dfa

---
+++ modules/hrv_dfa.jl
@@ -0,0 +1,74 @@
+module hrv_dfa
+	function polyfit(x, y, polyDegree)
+
+		m = zeros(length(x), polyDegree+1);
+
+		for i=1:polyDegree+1
+			for j=1:length(x)
+				if i == 1
+					m[j,i] = 1;
+				elseif i == 2
+					m[j,i] = x[j];
+				else
+					m[j,i] = x[j]^(i-1);
+				end
+			end
+		end
+		g = m'*m;
+		a = pinv(g)*m'*y;
+
+		return a;
+	end
+
+	function dfa(signal)
+		l = length(signal);
+		RR = signal[1:(l - 1)];
+		RRsr = mean(signal);
+
+		N = 10;
+		y = 0;
+		yTmp = 0;		
+		
+		iStop = length(RR);
+		for i=1:iStop
+			tmpRR = RR[i];
+			yTmp = sum((tmpRR - RRsr).^2);
+			y = [y yTmp];
+		end
+		
+		n = length(y);
+
+		nn = 0;
+		ff = 0;
+		trendlines = zeros(n);
+		
+		while n > 2
+			for i=1:n:length(y)-n
+				p = polyfit(i:i+n-1, y[i:i+n-1], 1);
+				tmpTrendlines = p[2]+p[1]*(i);
+				trendlines[i] = tmpTrendlines;
+			end
+			lt = length(trendlines);
+			yyy = y[1:lt];
+
+			F = sqrt(1/n*sum((yyy-trendlines).^2));
+			n = floor(n/2);
+			    
+			nn = [n nn];
+			ff = [F ff];
+		end
+		
+		nn = nn[2:length(nn)-2];
+		ff = ff[2:length(ff)-2];
+		
+		logF = log(ff);
+		logN = log(nn);
+		
+		a = zeros(length(logF));
+		for i=1:length(logF)
+			a[i] = logF[i]/logN[i];
+		end
+
+		return a;
+	end
+end
